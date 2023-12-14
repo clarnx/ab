c27565df7c8a16d9a4cc3bcf6d5ae14f42bb28c9
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-12-14 19:21:49 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = { reactStrictMode: false, output: 'export', };
 
 module.exports = nextConfig;
 
